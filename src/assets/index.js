5e90ffe65700d069f42ee09d54fdf8ec3241a9ab
JoshuaToback <joshuatoback@gmail.com>
2023-03-29 16:47:54 -0400

    Added new logo and began implementing components

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import jtlogo from "./jtlogo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -31,7 +31,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
+  jtlogo,
   backend,
   creator,
   mobile,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../style'
+import { navLinks } from '../constants'
+import {jtlogo, menu, close } from "../assets"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav 
+    className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to="/" className='flex items-center gap-2' onClick={() => {
+          setActive(""); 
+          window.scrollTo(0,0);}}>
+            <img src={jtlogo} alt="logo" className='w-9 h-9 contain' />
+          </Link>
+          <p className='text-red-500'>test</p>
+      </div>
+    </nav>
   )
 }
 
